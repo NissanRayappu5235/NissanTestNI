d1425257f6b463487f04610ccc5b40f825a82a9c
nissan <r.nissan2010@gmail.com>
2022-06-16 18:36:47 -0400

    Creating Initial commit

---
+++ NissanTestAB/src/main/java/nissan/rayappu/n01435235/n/Nissan1Activity.java
@@ -0,0 +1,14 @@
+package nissan.rayappu.n01435235.n;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Nissan1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
